6094ec690f559e5b2bb684cf13305753c7e1b503
Elex <developer@elex-project.com>
2021-01-18 15:54:33 +0900

    # 2.4.2

--- buildSrc/src/main/kotlin/base-library.gradle.kts
+++ buildSrc/src/main/kotlin/base-library.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.elex-project.amarok"
-version = "2.4.1"
+version = "2.4.2"
 
 repositories {
 	maven {
@@ -17,7 +17,7 @@ dependencies {
 	implementation("org.slf4j:slf4j-api:1.7.30")
 	implementation("org.jetbrains:annotations:20.1.0")
 
-	api("com.elex-project:abraxas:4.0.2")
+	api("com.elex-project:abraxas:4.0.3")
 	api("com.elex-project:http-companion:2.1.1")
 
 	implementation("com.fasterxml.jackson.core:jackson-databind:2.12.1")
